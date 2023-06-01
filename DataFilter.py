9ac832e6b51d474101ad7762eabbf09dd4271932
sroze <srozendom@gmail.com>
2023-06-01 10:01:57 +0200

    datagathering almost complete

--- DataFilter.py
+++ DataFilter.py
@@ -3,13 +3,13 @@ import pandas as pd
 import numpy
 import scipy.signal as signal
 from DataInput import Data_Input
-
 class Data_filter:
 
     def __init__(self):
         self.data_input = Data_Input()
         # Load data
         self.sensors = ['6AAE', '7786']
+        self.test = 0
     def run(self):
         #df_tibia['norm'] = numpy.sqrt(df_tibia['acc_x']*df_tibia['acc_x'] + df_tibia['acc_y']*df_tibia['acc_y'] + df_tibia['acc_z']*df_tibia['acc_z'])
         # df_head['norm'] = numpy.sqrt(df_head['acc_x']*df_head['acc_x'] + df_head['acc_y']*df_head['acc_y'] + df_head['acc_z']*df_head['acc_z'])
@@ -18,42 +18,41 @@ class Data_filter:
         values = self.data_input.get_values()
         sternum = [values[self.sensors[0]]['acc_x'],values[self.sensors[0]]['acc_y'],values[self.sensors[0]]['acc_z'],[]]
         tibia = [values[self.sensors[1]]['acc_x'],values[self.sensors[1]]['acc_y'],values[self.sensors[1]]['acc_z'],[]]
-        print(sternum)
-        print(tibia)
+
+        # calculate the norm
         for i in range(len(sternum[0])):
-            print("sternum" + str(i))
-            sternum[3][i] = numpy.sqrt(sternum[0][i]*sternum[0][i] + sternum[1][i]*sternum[1][i] + sternum[2][i]*sternum[2][i])
+            sternum[3].append(numpy.sqrt(sternum[0][i]*sternum[0][i] + sternum[1][i]*sternum[1][i] + sternum[2][i]*sternum[2][i]))
         for i in range(len(tibia[0])):
-            print("tibia"+ str(i))
-            tibia[3][i] = numpy.sqrt(tibia[0][i] * tibia[0][i] + tibia[1][i] * tibia[1][i] + tibia[2][i] * tibia[2][i])
+            tibia[3].append(numpy.sqrt(tibia[0][i] * tibia[0][i] + tibia[1][i] * tibia[1][i] + tibia[2][i] * tibia[2][i]))
 
+        # find peaks
+        peaks_s = signal.find_peaks(sternum[3],1, distance=30)
+        peaks_t = signal.find_peaks(tibia[3],1, distance=15)
 
-
-
-        #find peaks
-        peaks_l = signal.find_peaks(sternum[0],1, distance=300)
-        peaks_h = signal.find_peaks(tibia[0],1, distance=150)
-
-        #seperate data for calculation
-        peak_h_indecies, peak_h_values = peaks_h
-        peak_h_values = peak_h_values["peak_heights"]
-        peak_l_indecies, peak_l_values = peaks_l
-        peak_l_values = peak_l_values["peak_heights"]
+        # seperate data for calculation
+        peak_s_indecies, peak_s_values = peaks_s
+        peak_s_values = peak_s_values["peak_heights"]
+        peak_t_indecies, peak_t_values = peaks_t
+        peak_t_values = peak_t_values["peak_heights"]
         # print(len(peak_h_indecies))
-        peaks_h_corrected = []
-        peaks_l_corrected = []
+        peaks_s_corrected = []
+        peaks_t_corrected = []
 
-        #filter head peaks
-        for h in range(len(peak_h_values)):
-            for l in range(len(peak_l_values)):
-                if 0 < (peak_h_indecies[h] - peak_l_indecies[l]) < 150:
-                    peaks_h_corrected.append(peak_h_values[h])
-                    peaks_l_corrected.append(peak_l_values[l])
+        # filter sternum peaks
+        for s in range(len(peak_s_values)):
+            for t in range(len(peak_t_values)):
+                if 0 < (peak_s_indecies[s] - peak_t_indecies[t]) < 15:
+                    peaks_s_corrected.append(peak_s_values[s])
+                    peaks_t_corrected.append(peak_t_values[t])
                     break
 
-        norms_t = peaks_l_corrected
-        norms_h = peaks_h_corrected
-        #print(float(numpy.mean(numpy.array(norms_h)))/float(numpy.mean(numpy.array(norms_t))))
+        norms_t = peaks_t_corrected
+        norms_s = peaks_s_corrected
+        print("-----------------------------------------------------------------------------------------------------------------")
+        print(float(numpy.mean(numpy.array(norms_s)))/float(numpy.mean(numpy.array(norms_t))))
+        print(len(peaks_s[0])/20)
+
+
 
 programm = Data_filter()
 programm.data_input.start(programm.sensors)
