bc1c6cc8caad2765cf416a5eb3f141732bc2144d
sroze <srozendom@gmail.com>
2023-06-05 15:58:17 +0200

    updated main and handler

--- Music_handler.py
+++ Music_handler.py
@@ -1,1 +1,20 @@
 # all the music processing needs to be done before this can be made
+
+from songPicker import SongPicker
+
+class Music_Handler:
+    def __init__(self):
+        self.song_picker = SongPicker()
+
+    def play_song(self):
+        self.song_picker.next_song()
+        song = self.song_picker.get_song()
+        # song = self.speedajustment(song)
+        # self.song_player.play(song)
+
+    def loop(self,target_BPM, attenuation):
+        self.song_picker.ajust_queue(target_BPM)
+        #self.noiseadder.addnoise(attenuation)
+
+
+
--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 # Code written by Emilie, Fran, Jules, Julia, Nienke and Sven
 from DataFilter import Data_filter
+from Music_handler import Music_Handler
 
+handler = Music_Handler()
 data = Data_filter(['6AAE', '7786'])
+goal_BPM = 180
 data.data_input.start(data.sensors)
+song_end = True
 while True:
     attenuation, cadence = data.run()
     print("-----------------------------------------------------------------------------------------------------------------")
     print(attenuation)
     print(cadence)
+    if song_end:
+        handler.play_song()
+    target_BPM = cadence + (goal_BPM - cadence / 10) # subject to change
+    handler.loop(target_BPM,attenuation)
